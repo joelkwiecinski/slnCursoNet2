aa5e06274331ec16d3d569c4a8dbbeee7972195f
Joe Kwiecinski <joekwiecinski@outlook.com>
2022-11-02 21:19:10 -0300

    Agregar archivos de proyecto.

---
+++ WindowsApp/Entidades/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApp.Entidades
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal PrecioCosto { get; set; }
+        public double Margen { get; set; }
+        public double IVA { get; set; }
+        public decimal PrecioBruto { get { return PrecioCosto + Convert.ToDecimal(Margen); } }
+        public decimal PrecioVenta { get { return PrecioBruto * Convert.ToDecimal(IVA + 1); } }
+        public string Proveedor { get; set; }
+        public string Categoria { get; set; }
+        public string SubCategoria { get; set; }
+
+    }
+}
